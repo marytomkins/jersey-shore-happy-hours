3803b2fed792b70d09c3ea68a86e12714f940dcb
mct125 <mct125@scarletmail.rutgers.edu>
2025-05-18 21:58:57 -0400

    feedback/footer

---
+++ src/components/Feedback.js
@@ -0,0 +1,68 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const Feedback = () => {
+  const [formText, setFormText] = useState("");
+
+  const handleChange = (e) => {
+    let value = e?.target?.value || "";
+    setFormText(value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("https://formspree.io/f/xvgayopb", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: formText,
+        }),
+      });
+
+      if (response.ok) {
+        alert("Thank you! Your feedback has been submitted.");
+      } else {
+        alert("Something went wrong. Please try again.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while submitting.");
+    }
+    setFormText("");
+  };
+
+  return (
+    <div className="p-6 md:w-1/2 w-3/4 mx-auto bg-white rounded-md mt-8">
+      <div className="text-2xl font-semibold text-center">Submit Feedback</div>
+      <div className="text-sm mb-4 px-4 text-center">
+        If there are any mistakes or I am missing any restaurants/bars, please
+        let me know below!
+      </div>
+      <form className="space-y-4">
+        <textarea
+          type="text"
+          name="formText"
+          value={formText}
+          onChange={(e) => handleChange(e)}
+          placeholder="Add your feedback here..."
+          className="w-full px-3 py-2 border rounded"
+          rows={6}
+        />
+      </form>
+      <div className="flex justify-end mt-4">
+        <button
+          type="submit"
+          className="bg-[#2f55c4] hover:bg-[#b4e255] text-sm text-white px-8 py-2 rounded"
+          onClick={(e) => {
+            if (formText.length > 0) handleSubmit(e);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Feedback;
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = ({ onClick, showFeedbackForm }) => {
+  return (
+    <div className="block text-center text-xs py-4">
+      <div
+        className="underline cursor-pointer hover:text-gray-300"
+        onClick={() => onClick()}
+      >
+        {showFeedbackForm ? "Home Page" : "Feedback Form"}
+      </div>
+      <div className="mt-2">
+        Jersey Shore Happy Hours © Developed by MT 2025
+      </div>
+    </div>
+  );
+};
+export default Footer;
